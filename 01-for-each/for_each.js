9db91f8639d75f92daafb74a0079d1bc54ab17d9
ZubairGhani <ghani.zubairmohammad@gmail.com>
2021-06-23 10:17:01 +0200

    for_each is finsihed

--- 01-for-each/for_each.js
+++ 01-for-each/for_each.js
@@ -1,5 +1,12 @@
-// define an array of fruits
 
+// define an array of fruits
+const fruitArray = ["Apple" , "Banana", "Kiwi", "Watermelon", "oranage"];
 // .forEach((fruit, index) => {
+    fruitArray.forEach((fruit, index) => {
+        
     // console.log() a message interpolating the name of the fruit and it's position in the array (index increased by 1);
-// })
+    console.log(`${index + 1} ${fruit}`)
+});
+
+
+
